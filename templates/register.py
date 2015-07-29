ca1be2bace94a6337c5d2cf7178c56738ed6b8c0
wqythu13 <wangqinyu303@gmail.com>
2015-07-29 19:12:23 +0800

    ...

--- templates/register.py
+++ templates/register.py
@@ -6,19 +6,17 @@ html = """
 <TITLE>Register OK!.py</TITLE>
 <H1>Greetings</H1>
 <HR>
-<P>%(name)s</P>
 <P>%(nick-name)s</P>
 <P>%(e-mail)s</P>
 
 <HR>"""
 def htmlize(adict):
     new = {}
-    new['name'] = ('Hello, %s.' % adict['name'].value)
     new['nick-name'] = ('您的昵称：%s' % adict['nick-name'].value)
     new['e-mail'] = ('您的邮箱地址：%s' % adict['e-mail'].value)
     return new
 
-if not 'name' in form:
-    print(html % {'name':'注册失败，大侠请重新来过!', 'nick-name':'', 'e-mail':''})
+if not 'nick-name' in form:
+    print(html % {'nick-name':'注册失败，大侠请重新来过!', 'e-mail':''})
 else:
     print(html % htmlize(form))
