576fd3168249e52805207d038f56cbabf93f73c8
zhangyitian13 <zytdark@126.com>
2015-07-31 20:37:36 +0800

    vv

--- AsiaTube/views.py
+++ AsiaTube/views.py
@@ -48,17 +48,26 @@ def register(request):
      #   )
      #   p.save()
 
-def handle_uploaded_file(f):#'F:/AsiaTube/Video/'+ str(12) + '_' +
-    with open('F:/AsiaTube/Video/'+ str(12306) + '_'+ f.name, 'wb+') as info:
+def handle_uploaded_file(f, x):#'F:/AsiaTube/Video/'+ str(12) + '_' +
+    with open('F:/AsiaTube/Video/'+ str(x) + '_'+ f.name, 'wb+') as info:
+        for chunk in f.chunks():
+            info.write(chunk)
+    return f
+
+def handle_uploaded_pic(f, x):#'F:/AsiaTube/Video/'+ str(12) + '_' +
+    with open('F:/AsiaTube/Icon/'+ str(x) + '_'+ f.name, 'wb+') as info:
         for chunk in f.chunks():
             info.write(chunk)
     return f
 def uploadvideo(request):
+    if 'id' not in request.COOKIES:
+        return render_to_response("login.html")
     if request.method == 'GET':
         return render_to_response("update.html", context_instance=RequestContext(request))
     else:
         a = request.FILES['file']
-        handle_uploaded_file(a)
+        x = request.COOKIES['id']
+        handle_uploaded_file(a, x)
         #form = UploadFileForm(request.POST, request.FILES)
         #if form.is_valid():
         #    return HttpResponse("You have upload your file successfully")
@@ -66,7 +75,34 @@ def uploadvideo(request):
         #print(form.title)
 
         return render_to_response("video.html",{
-            'UserID':12306,
+            'UserID':x,
             'Video_title':'萌杰打飞机',
             'Video_src':'http://192.168.1.103:8000/static/1.mp4'
         }, context_instance=RequestContext(request))
+
+def ModifyInfo(request):
+    if 'id' not in request.COOKIES:
+        return HttpResponse("How can you find this page!!!")
+    id = request.COOKIES['id']
+    iuser = IUser()
+    user = iuser.SelectById(id)
+    if user == None:
+        return HttpResponse("You have been deleted")
+    if request.method == 'GET':
+        return render_to_response("modifyinfo.html", {
+            'UserID':id,
+            'oldname':user.name,
+            'oldemail':user.email,
+        },context_instance=RequestContext(request))
+    else:
+        nick_name = request.POST.get("nick-name")
+        email = request.POST.get("email")
+        file = request.FILES['file']
+        a = IUser()
+        a.ModifyEmail(id, email)
+        a.ModifyName(id, nick_name)
+        a.ModifyImg(id, file.name)
+        handle_uploaded_pic(file, id)
+        return HttpResponse("You have modified successfully!1")
+        #upload iamge
+
